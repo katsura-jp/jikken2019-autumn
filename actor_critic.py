cacb2d8c9871121c64d48e10502f194207992c59
katsura_jp <nok.addayo@gmail.com>
2019-10-15 17:51:31 +0900

    upload actor-critic training file

---
+++ actor_critic.py
@@ -0,0 +1,36 @@
+import os
+import random
+import datetime
+import time
+import argparse
+import logging
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import gym
+
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+class CriticLoss(nn.Module):
+    def __init__(self):
+        super(CriticLoss, self).__init__()
+        pass
+    def forward(self):
+        pass
+
+class ActorLoss(nn.Module):
+    def __init__(self):
+        super(ActorLoss, self).__init__()
+        pass
+    def forward(self):
+        pass
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
